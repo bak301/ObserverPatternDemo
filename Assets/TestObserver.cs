d2806b0c86f563def7d590c77e240aa477f17717
T.Bach <soulbodyx@gmail.com>
2021-01-06 10:32:30 +0700

    Add Test UI to test Events

---
+++ Assets/TestObserver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestObserver : MonoBehaviour
+{
+    [SerializeField] private Dropdown events;
+    [SerializeField] private Button trigger;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        events.ClearOptions();
+        events.AddOptions(new List<string>(Enum.GetNames(typeof(GameEvent))));
+        trigger.onClick.AddListener(() => 
+        {
+            GameEvent ev = (GameEvent)events.value;
+            this.FireEvent(ev);
+        });
+    }
+}
